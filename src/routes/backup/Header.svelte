9ba83b9747d1197006fb397d4ebd893d3cb8fc5e
YZhLu <luiz.zito@icloud.com>
2025-07-24 11:09:20 -0300

    bug fix

--- src/routes/backup/Header.svelte
+++ src/routes/backup/Header.svelte
@@ -1,4 +1,4 @@
-<script lang="ts">
+<!-- <script lang="ts">
 	import { page } from '$app/state';
 	import logo from '$lib/images/svelte-logo.svg';
 	import github from '$lib/images/github.svg';
@@ -126,4 +126,4 @@
 	a:hover {
 		color: var(--color-theme-1);
 	}
-</style>
+</style> -->
